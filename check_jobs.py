8e57be986fc0c231035de182040fbe12b6207079
sgossage <seth.gossage@cfa.harvard.edu>
2017-07-06 03:07:25 -0400

    Added ability to resume runs stopped at HB to 1st TP in check_jobs.py. Fixed mesafiles/ inlist files to include 1st as the default stopping pt.

--- check_jobs.py
+++ check_jobs.py
@@ -4,6 +4,8 @@ from glob import glob
 import os
 import sys
 import subprocess
+import shutil
+from distutils.dir_util import copy_tree
 
 def checkruns(runname_str, out_sacct=True):
 
@@ -54,7 +56,7 @@ def checkruns(runname_str, out_sacct=True):
             print(subprocess.check_output(['sacct', '--jobs={:s}'.format(jobids), '--format=JobID,JobName,Partition,AllocCPUS,State,ExitCode,MaxRSS,Elapsed']))
 
         # Print number of failures and which masses will be re-sumitted:
-        print("{:s} had {:d} tracks with fatal errors.".format(targetdir_of(rundir), len(failed_massdirs)))
+        print("{:s} had {:d} tracks with fatal errors.".format(target_of(rundir), len(failed_massdirs)))
         for failed_massdir in failed_massdirs:
             print("{:.2f} Msol will be resubmitted.".format(massof(failed_massdir.split('/')[-1])))
 
@@ -64,6 +66,107 @@ def checkruns(runname_str, out_sacct=True):
 
     return failed_dict
 
+def getphotos(runname_str, TPextend=True):
+
+    # out_sacct can't be turned off if using the script currently; will change.
+
+    rundirs = glob(runname_str)
+
+    res_dict = {}
+
+    for rundir in rundirs:
+        badtracks = []
+        trackdirs = glob(os.path.join(rundir, '*M*dir'))
+        for trackdir in trackdirs:
+            # Checks for the final photo of a track:
+
+            # .o file is used to recover the final photo of the MESA run.
+            try:
+                outfile = glob(os.path.join(trackdir, '*.o'))[0]
+            except IndexError:
+                print("{:s} is missing a SLURM .o (i.e., output) file.".format(trackdir))
+                continue
+
+            with open(outfile, 'r') as of:
+                outlines = of.readlines()
+                for i, line in enumerate(outlines):
+                    if 'save photos/x' in line:
+                        if TPextend == True & ('termination code: HB_limit' in outlines[i+1]):
+                            # check if extension to TP is necc.
+                            lastphoto = line.split('photos/')[-1].split(' ')[0]
+                            #photos.append((target_of(trackdir), lastphoto))
+                            badtracks.append(trackdir)
+                            # Make a SLURM submission script to resume the job.
+                            # first copying the XXXXXM_run.sh script to make a new script that will resume the run:
+                            runscript_path = os.path.join(trackdir, target_of(trackdir).replace('dir', 'run.sh'))
+                            resscript_path = os.path.join(trackdir, target_of(runscript_path).replace('run', 'resume'))
+                            # modify the 'resume' script:
+                            with open(resscript_path, 'w+') as resscript:
+                                # only need first 11 lines of runscript.
+                                with open(runscript_path, 'r') as runscript:
+                                    scriptlines = runscript.readlines()[:12]
+                                    scriptlines[7] = '#SBATCH -o 00100Mre.o\n'
+                                    scriptlines[8] = '#SBATCH -e 00100Mre.e\n'
+                                # In 'resume' script, insert command to resume the run using the last photo:
+                                scriptlines.append('./re {:s}\n'.format(lastphoto))
+                                resscript.writelines(scriptlines)
+
+                            # Now make sure the desired stopping condition is placed in the inlist:
+                            inlist_path = os.path.join(trackdir, 'inlist_project')
+                            with open(inlist_path, 'r') as inlist:
+                                inlistlines = inlist.readlines()
+
+                            # add in new stopping condition and comment out the old one.
+                            condition_infile = False
+                            condition = 'stop_at_TP = .true.'
+                            for j, inlistline in enumerate(inlistlines):
+                                if '!gamma_center_limit' in inlistline:
+                                    inlistlines[j] = inlistline.replace('!', '')
+                               
+                                if isuncommented_and_contains(inlistline, 'HB_limit', '!'):
+                                    inlistlines[j] = inlistline.replace('HB_limit', '!HB_limit')
+                                    # place the new stopping condition in after this line:
+                                    insertion_idx = j+1
+                                if 'stop_at_TP' in inlistline:
+                                    condition_infile = True
+                                    break
+
+                            try:
+                                if not condition_infile:
+                                    # len(condition) + 7 has '7' b/c of 5 prepended spaces and two chars for '\n'
+                                    inlistlines.insert(insertion_idx, ('{:s}\n'.format(condition)).rjust(len(condition)+7))
+                            except NameError:
+                                # 'insertion_index' was not defined.
+                                print('{:s} did not have an uncommented \'HB_limit\' line.'.format(inlist_path))
+
+                            # Now write the lines to a new inlist file:
+                            newinlist_path = os.path.join(trackdir, 'inlist')
+                            with open(newinlist_path, 'w+') as newinlist:
+                                newinlist.writelines(inlistlines)
+                            # replace old project_inlist too:
+                            with open(inlist_path, 'r+') as inlist:
+                                inlist.writelines(inlistlines)
+                            # copy the re executable from MESAWORK_DIR.
+                            shutil.copy(os.path.join(os.environ['MESAWORK_DIR'], 'cleanworkdir/re'), trackdir)
+                            copy_tree(os.path.join(os.environ['MESAWORK_DIR'], 'cleanworkdir/src'), os.path.join(trackdir, 'src'))
+
+        if len(badtracks) > 0:
+            res_dict[rundir] = badtracks
+            print("{:d}/{:d} tracks require extension in {:s}.".format(len(badtracks), len(trackdirs), target_of(rundir)))
+        else:
+            print("No tracks in {:s} require extension.".format(target_of(rundir)))
+        print("===========================================================")
+
+    return res_dict
+
+def isuncommented_and_contains(string, phrase, comment_char='!'):
+
+    comment_pos = string.find(comment_char)
+    phrase_pos = string.find(phrase)
+    
+    # if (phrase is in srting) AND (e.g. the namelist comment '!' is not in the line OR comes after the phrase):
+    return (phrase in string) & ( (comment_pos < 0) | (comment_pos > phrase_pos) )
+
 def massof(trackname):
 
     # converts MIST track dir names into their resp. masses.
@@ -72,7 +175,7 @@ def massof(trackname):
 
     return float(mass_str)/100.0
 
-def targetdir_of(path):
+def target_of(path):
   
     # returns the final directory name of a given path.
 
@@ -101,26 +204,46 @@ if __name__ == '__main__':
 
     # string for the run names to check; may contain wildcards. E.g. 'feh*TP'.
     runname_str = sys.argv[1]
+    mode = sys.argv[2]
 
     #prepend MIST grid path to runs:
     runname_str = os.path.join(os.environ['MIST_GRID_DIR'], 'MIST_v1.0', runname_str)
 
-    # now check the runs for failed tracks:
-    failed_dict = checkruns(runname_str)
+    if mode == 'rerun':
+        # now check the runs for failed tracks:
+        failed_dict = checkruns(runname_str)
 
-    # Go through various run directories found...
-    for rundir in failed_dict:
-        # If there were failed tracks, re-submit them.
-        failed_massdirs = failed_dict[rundir]
-        print("___________________________________________________________")
-        print("In {:s} re-submitting {:d} jobs.".format(targetdir_of(rundir), len(failed_massdirs)))
+        # Go through various run directories found...
+        for rundir in failed_dict:
+            # If there were failed tracks, re-submit them.
+            failed_massdirs = failed_dict[rundir]
+            print("___________________________________________________________")
+            print("In {:s} re-submitting {:d} jobs.".format(target_of(rundir), len(failed_massdirs)))
 
-        # Re-submit the failed tracks:
-        for failed_massdir in failed_massdirs:
-            os.chdir(failed_massdir)
-            print("re-submitting {:s}...".format(targetdir_of(failed_massdir)))
-            slurmfile = glob(os.path.join(failed_massdir, '*M_run.sh'))[0]
-            print(slurmfile)
-            #os.system("sbatch " + slurmfile)
-            #os.chdir(os.environ['MIST_CODE_DIR'])
+            # Re-submit the failed tracks:
+            for failed_massdir in failed_massdirs:
+                os.chdir(failed_massdir)
+                print("re-submitting {:s}...".format(target_of(failed_massdir)))
+                slurmfile = glob(os.path.join(failed_massdir, '*M_run.sh'))[0]
+                print(slurmfile)
+                #os.system("sbatch " + slurmfile)
+                os.chdir(os.environ['MIST_CODE_DIR'])
 
+    elif mode == 'resume':
+        res_dict = getphotos(runname_str)
+        for rundir in res_dict:
+            resume_dirs = res_dict[rundir]
+
+            # Resume tracks found:
+            print("___________________________________________________________")
+            print("In {:s} resuming {:d} jobs.".format(target_of(rundir), len(resume_dirs)))
+
+            # Re-submit the failed tracks:
+            for resume_dir in resume_dirs:
+                os.chdir(resume_dir)
+                print("resuming {:s}...".format(target_of(resume_dir)))
+                slurmfile = glob(os.path.join(resume_dir, '*M_resume.sh'))[0]
+                #print(slurmfile)
+                os.system("sbatch "+slurmfile)
+                os.chdir(os.environ['MIST_CODE_DIR'])
+                
